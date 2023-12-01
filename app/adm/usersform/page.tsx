3902fec75301e954771030156c2992c297048ac3
Mumoide <restevez1993@gmail.com>
2023-11-30 21:34:45 -0300

    fix: security added to passwords

--- app/adm/usersform/page.tsx
+++ app/adm/usersform/page.tsx
@@ -164,8 +164,10 @@ export default function App() {
                     message: "Min length is 5 characters",
                   },
                   pattern: {
-                    value: /^[^^';"<>-]*$/,
-                    message: "Invalid character used",
+                    value:
+                      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])[^'^";<>-]*$/,
+                    message:
+                      "Se requiere una mayúscula, una minúscula y un caracter especial. (Se excluyen los caracteres: ^';\"<>-)",
                   },
                 })}
                 className="h-50 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
